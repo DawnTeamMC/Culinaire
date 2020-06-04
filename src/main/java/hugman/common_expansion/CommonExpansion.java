4b6b5204fb32f226a43dc29706cdd1a07f795049
Hugman <contact.hugman@gmail.com>
2020-06-04 18:18:36 +0200

    Initial commit

---
+++ src/main/java/hugman/common_expansion/CommonExpansion.java
@@ -0,0 +1,12 @@
+package hugman.common_expansion;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CommonExpansion implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+		System.out.println("Hello Fabric world!");
+	}
+}
