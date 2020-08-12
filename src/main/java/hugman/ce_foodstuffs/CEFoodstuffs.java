4fb4ac5325f0b1d8925c60f3a9172ad631b0bc3a
Hugman <contact.hugman@gmail.com>
2020-08-12 15:21:58 +0200

    Added some items to vanilla loot tables

--- src/main/java/hugman/ce_foodstuffs/CEFoodstuffs.java
+++ src/main/java/hugman/ce_foodstuffs/CEFoodstuffs.java
@@ -3,6 +3,7 @@ package hugman.ce_foodstuffs;
 import hugman.ce_foodstuffs.init.CEFBlocks;
 import hugman.ce_foodstuffs.init.CEFItems;
 import hugman.ce_foodstuffs.init.data.CEFBlockEntityTypes;
+import hugman.ce_foodstuffs.init.data.CEFLootTables;
 import hugman.ce_foodstuffs.init.data.CEFRecipeSerializers;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
@@ -19,6 +20,7 @@ public class CEFoodstuffs implements ModInitializer {
 		new CEFBlockEntityTypes();
 		new CEFItems();
 		new CEFRecipeSerializers();
+		CEFLootTables.addToVanillaTables();
 	}
 
 	public static Identifier id(String name) {
