aef9310ad74ef0f1bbddef1f887e399a964d2749
Hugman <contact.hugman@gmail.com>
2020-08-29 14:42:54 +0200

    Added remainders for salad, mashed potatoes and marshmallows on sticks for consuming and using in recipe

--- src/main/java/hugman/ce_foodstuffs/init/CEFItems.java
+++ src/main/java/hugman/ce_foodstuffs/init/CEFItems.java
@@ -4,10 +4,7 @@ import hugman.ce_foodstuffs.CEFoodstuffs;
 import hugman.ce_foodstuffs.init.data.CEFFoods;
 import hugman.ce_foodstuffs.objects.item.MarshmallowOnAStickItem;
 import hugman.ce_foodstuffs.objects.item.SandwichItem;
-import net.minecraft.item.AliasedBlockItem;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraft.util.registry.Registry;
 
 public class CEFItems {
@@ -20,13 +17,25 @@ public class CEFItems {
 	public static final Item MARSHMALLOW = register("marshmallow", new Item(new Item.Settings().group(ItemGroup.FOOD).food(CEFFoods.UNCOOKED_MARSHMALLOW)));
 	public static final Item APPLE_PIE = register("apple_pie", new Item(new Item.Settings().group(ItemGroup.FOOD).food(CEFFoods.APPLE_PIE)));
 	public static final Item SWEET_BERRY_PIE = register("sweet_berry_pie", new Item(new Item.Settings().group(ItemGroup.FOOD).food(CEFFoods.SWEET_BERRY_PIE)));
-	public static final Item SALAD = register("salad", new Item(new Item.Settings().group(ItemGroup.FOOD).maxCount(1).food(CEFFoods.SALAD)));
-	public static final Item MASHED_POTATOES = register("mashed_potatoes", new Item(new Item.Settings().group(ItemGroup.FOOD).food(CEFFoods.MASHED_POTATOES).maxCount(1)));
+	public static final Item SALAD = register("salad", new MushroomStewItem(new Item.Settings().group(ItemGroup.FOOD).maxCount(1).food(CEFFoods.SALAD)));
+	public static final Item MASHED_POTATOES = register("mashed_potatoes", new MushroomStewItem(new Item.Settings().group(ItemGroup.FOOD).food(CEFFoods.MASHED_POTATOES).maxCount(1)));
 	public static final Item SANDWICH = register("sandwich", new SandwichItem(new Item.Settings().group(ItemGroup.FOOD).food(CEFFoods.SANDWICH_BASE).maxCount(1)));
-	public static final Item UNCOOKED_MARSHMALLOW_ON_A_STICK = register("uncooked_marshmallow_on_a_stick", new MarshmallowOnAStickItem(new Item.Settings().group(ItemGroup.FOOD).food(CEFFoods.UNCOOKED_MARSHMALLOW).maxCount(1)));
-	public static final Item TOASTY_MARSHMALLOW_ON_A_STICK = register("toasty_marshmallow_on_a_stick", new MarshmallowOnAStickItem(new Item.Settings().group(ItemGroup.FOOD).food(CEFFoods.TOASTY_MARSHMALLOW).maxCount(1)));
-	public static final Item GOLDEN_MARSHMALLOW_ON_A_STICK = register("golden_marshmallow_on_a_stick", new MarshmallowOnAStickItem(new Item.Settings().group(ItemGroup.FOOD).food(CEFFoods.GOLDEN_MARSHMALLOW).maxCount(1)));
-	public static final Item BURNT_MARSHMALLOW_ON_A_STICK = register("burnt_marshmallow_on_a_stick", new MarshmallowOnAStickItem(new Item.Settings().group(ItemGroup.FOOD).food(CEFFoods.BURNT_MARSHMALLOW).maxCount(1)));
+	public static final Item UNCOOKED_MARSHMALLOW_ON_A_STICK = register("uncooked_marshmallow_on_a_stick", new MarshmallowOnAStickItem(new Item.Settings().group(ItemGroup.FOOD)
+																																						  .food(CEFFoods.UNCOOKED_MARSHMALLOW)
+																																						  .maxCount(1)
+																																						  .recipeRemainder(Items.STICK)));
+	public static final Item TOASTY_MARSHMALLOW_ON_A_STICK = register("toasty_marshmallow_on_a_stick", new MarshmallowOnAStickItem(new Item.Settings().group(ItemGroup.FOOD)
+																																					  .food(CEFFoods.TOASTY_MARSHMALLOW)
+																																					  .maxCount(1)
+																																					  .recipeRemainder(Items.STICK)));
+	public static final Item GOLDEN_MARSHMALLOW_ON_A_STICK = register("golden_marshmallow_on_a_stick", new MarshmallowOnAStickItem(new Item.Settings().group(ItemGroup.FOOD)
+																																					  .food(CEFFoods.GOLDEN_MARSHMALLOW)
+																																					  .maxCount(1)
+																																					  .recipeRemainder(Items.STICK)));
+	public static final Item BURNT_MARSHMALLOW_ON_A_STICK = register("burnt_marshmallow_on_a_stick", new MarshmallowOnAStickItem(new Item.Settings().group(ItemGroup.FOOD)
+																																					.food(CEFFoods.BURNT_MARSHMALLOW)
+																																					.maxCount(1)
+																																					.recipeRemainder(Items.STICK)));
 	public static final Item KETTLE = register("kettle", new BlockItem(CEFBlocks.KETTLE, new Item.Settings().group(ItemGroup.DECORATIONS)));
 
 	private static Item register(String name, Item item) {
