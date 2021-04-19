995ccda3cd9f1ece0cdeb0fee7c8d93422e13f2b
Hugman <contact.hugman@gmail.com>
2021-04-19 22:23:36 +0200

    Datafix ce_foodstuffs

---
+++ src/main/java/com/hugman/culinaire/mixin/DefaultedRegistryMixin.java
@@ -0,0 +1,20 @@
+package com.hugman.culinaire.mixin;
+
+import com.hugman.culinaire.Culinaire;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.DefaultedRegistry;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(DefaultedRegistry.class)
+public class DefaultedRegistryMixin {
+	@ModifyVariable(at = @At("HEAD"), method = "get(Lnet/minecraft/util/Identifier;)Ljava/lang/Object;", ordinal = 0)
+	Identifier fixMissingFromRegistry(@Nullable Identifier id) {
+		if(id != null) {
+			if(id.getNamespace().equals("ce_foodstuffs")) return new Identifier(Culinaire.MOD_DATA.getModName(), id.getPath());
+		}
+		return id;
+	}
+}
