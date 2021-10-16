1b610731000650383cc2b402cc5e4004f7dcdc48
Hugman <contact.hugman@gmail.com>
2021-10-16 23:48:06 +0200

    Updated deps

--- src/main/java/com/hugman/culinaire/init/CulinaireTeaBundle.java
+++ src/main/java/com/hugman/culinaire/init/CulinaireTeaBundle.java
@@ -6,14 +6,7 @@ import com.hugman.culinaire.objects.item.TeaBagItem;
 import com.hugman.culinaire.objects.item.TeaBottleItem;
 import com.hugman.culinaire.objects.recipe.TeaBagMakingRecipe;
 import com.hugman.culinaire.objects.screen.handler.KettleScreenHandler;
-import com.hugman.dawn.api.creator.BlockCreator;
-import com.hugman.dawn.api.creator.BlockEntityCreator;
-import com.hugman.dawn.api.creator.EffectCreator;
-import com.hugman.dawn.api.creator.ItemCreator;
-import com.hugman.dawn.api.creator.RecipeSerializerCreator;
-import com.hugman.dawn.api.creator.ScreenHandlerCreator;
-import com.hugman.dawn.api.creator.SoundCreator;
-import com.hugman.dawn.api.creator.StatCreator;
+import com.hugman.dawn.api.creator.*;
 import com.hugman.dawn.mod.object.effect.SimpleEffect;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
@@ -22,7 +15,7 @@ import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectType;
+import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
@@ -42,10 +35,10 @@ public class CulinaireTeaBundle extends CulinaireBundle {
 	public static final StatCreator KETTLE_INTERACTION_STAT = creator(new StatCreator("interact_with_kettle"));
 	public static final SoundCreator KETTLE_BREW_SOUND = creator(new SoundCreator("block.kettle.brew"));
 
-	public static final StatusEffect FORESIGHT = add(new EffectCreator("foresight", new SimpleEffect(StatusEffectType.BENEFICIAL, 9966823)));
-	public static final StatusEffect FULFILLMENT = add(new EffectCreator("fulfillment", new SimpleEffect(StatusEffectType.BENEFICIAL, 6048577)));
-	public static final StatusEffect GUARD = add(new EffectCreator("guard", new SimpleEffect(StatusEffectType.BENEFICIAL, 16440596)));
-	public static final StatusEffect POISON_RESISTANCE = add(new EffectCreator("poison_resistance", new SimpleEffect(StatusEffectType.BENEFICIAL, 7596722)));
+	public static final StatusEffect FORESIGHT = add(new EffectCreator("foresight", new SimpleEffect(StatusEffectCategory.BENEFICIAL, 9966823)));
+	public static final StatusEffect FULFILLMENT = add(new EffectCreator("fulfillment", new SimpleEffect(StatusEffectCategory.BENEFICIAL, 6048577)));
+	public static final StatusEffect GUARD = add(new EffectCreator("guard", new SimpleEffect(StatusEffectCategory.BENEFICIAL, 16440596)));
+	public static final StatusEffect POISON_RESISTANCE = add(new EffectCreator("poison_resistance", new SimpleEffect(StatusEffectCategory.BENEFICIAL, 7596722)));
 
 	public static void init() {
 	}
